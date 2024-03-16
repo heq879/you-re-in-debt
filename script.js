4f991e5f2c4c7857001a75d09ad9ebe6afcb286e
Glitch (glitch-hello-website) <none>
2024-03-16 20:50:29 +0000

    🚛🍅 Checkpoint ./script.js:64181659/1313

--- script.js
+++ script.js
@@ -7,43 +7,37 @@ console.log(thisExercise);
 
 
 
-// PART 1: Define a variable for each
+// PART A: Define a variable for each
 
-// A string with your name
+// 1. A string with your name
 
-// How many classes you're taking? – As a number
+// 2. How many classes you're taking? – As a number
 
-// How many pets do you have? – As a string
+// 3. How many pets do you have? – As a string
 
-// Are you at school today? Use a boolean 
+// 4. Are you at school today? Use a boolean 
 
-// An array of your 5 favorite animals
+// 5. An array of your 5 favorite animals
 
-// An array with your 3 favorite numbers
+// 6. An array with your 3 favorite numbers
 
-// An object about a movie that consists of its title, director, year, and if it's your favorite or not (using a boolean)
-
-
-
-
-// PART 2
-
-// Define a variable with your favorite dish (as a string)
-
-// Define a second variable where you combine a string and the food variable that you created, to say that the food is your favorite dish
-
-// Reuse your food variable, and change its value to a food that you don't like
-
-// Define a third variable where you combine a string that says "I don't like " and the food variable that you changed
+// 7. An object about a movie that consists of its title, director, year, and if it's your favorite or not (using a boolean)
 
 
 
-let myFood = 'noodles';
-console.log(myFood);
 
+// PART B
 
-let myFavorite = "My favorite dish is " + myFood;
-console.log(greetings);
+// 1. Define a variable with your favorite dish (as a string)
+
+// 2. Define a second variable where you combine a string and the food variable that you created, to say that the food is your favorite dish
+      // (Tip: As a result, you want the console to say something like: "My favorite dish is pizza")
+
+// 3. Reuse your food variable, and change its value to a food that you don't like
+
+// 4. Define a third variable where you combine a string and the food variable that you changed, to say that you don't like that dish
+      // (Tip: As a result, you want the console to say something like: "I don't like asparagus")
+
 
 
 
