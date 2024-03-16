2dddd7096aa61c6226f14d934400e6b2d837261a
Glitch (glitch-hello-website) <none>
2024-03-16 22:19:37 +0000

    🚶🐏 Checkpoint ./script.js:64181659/1477

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ console.log(thisExercise);
 
 
 
-// PART A: Define a variable for each
+// PART A: Defining variables – Define a variable for each:
 
 // 1. A string with your name
 
@@ -33,11 +33,12 @@ console.log(thisExercise);
 
 
 
-// PART B
+// PART B: Using variables – Console log each one as you answer the questions
 
 // 1. Define a variable with your favorite dish (as a string)
 
-// 2. Define a second variable where you combine a string and the food variable that you created, to say that the food is your favorite dish
+// 2. Define a second variable where you combine a string and the food variable that you created.
+// The goal is to have the console say that the food is your favorite dish
       // (Tip: As a result, you want the console to say something like: "My favorite dish is pizza")
 
 // 3. Reuse your food variable, and change its value to a food that you don't like
@@ -46,33 +47,40 @@ console.log(thisExercise);
       // (Tip: As a result, you want the console to say something like: "I don't like asparagus")
 
 
+let myDish = "noodles";
+let 
 
 
-// PART C
+
+// PART C: Math
 
 // 1. 
 
 
 
 
-// PART D
+// PART D: Arrays
 
-// 1. Create an array of 6 friends (their names) & console log it
+// 1. Create an array of 6 friends (their names) & console log it (inspect how it shows up, and the numbers given to each item in your array)
 
-// 2. Console log the first friend in your array
+// 2. Console log the first friend in your array using a number
 
-// 3. Console log the last friend in your array
+// 3. Console log the last friend in your array using a number
 
-// 4. Console log a random friend in your array
+// 4. Console log the number of items in your array (Tip: use array.length)
+
+// 5. Console log the first friend in your array again, but this time us array.length instead of a set number
+
+// 6. Console log a random friend in your array (Tip: You want to see the console logging different friends when you refresh the page)
 
 
-let animals = ["cats", "dogs", "birds"];
-console.log(animals);
-
-console.log(animals[0]);
-console.log(animals.length);
-console.log(animals[animals.length - 1]);
-console.log(animals[Math.floor(Math.random() * 3)]);
+let myFriends = ["John", "Lydia", "Shiv", "Esra", "Jin", "Melissa"];
+console.log(myFriends);
+console.log(myFriends[0]);
+console.log(myFriends[5]);
+console.log(myFriends.length);
+console.log(myFriends[myFriends.length - 1]);
+console.log(myFriends[Math.floor(Math.random() * myFriends.length)]);
                     
                     
                     
