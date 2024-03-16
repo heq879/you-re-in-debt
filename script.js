e7b37fbad5ec1f79beadd85d2eb62715f482b303
Glitch (glitch-hello-website) <none>
2024-03-16 20:28:09 +0000

    📙⛑ Checkpoint ./script.js:64181659/1035

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-// PART 1: Define a variable for & console log for each of the questions below.
+// Answer to the best of your abilities & console log for each of the questions below.
+
+let thisExercise = "Let's learn Javascript!";
+
+console.log(thisExercise);
+
+
+
+
+// PART 1: Define a variable for each
 
 // A string with your name
 
-// Number of classes you're taking
+// How many classes you're taking? – As a number
 
-// A string with the number of pets you have
+// How many pets do you have? – As a string
 
 // Are you at school today? Use a boolean 
 
@@ -16,6 +25,15 @@
 
 
 
-// PART 2: After answering each question below, console log.
+
+// PART 2
+
+// Define a variable named greetings with a greeting word (as a string)
+
+// Define a second variable where you're combining your first variable (greeting word) and a name that's a string
+
+// Now, reuse the
+
+// Define a second variable with a name
 
 // 
