df444eb96d61b0a0e5ab7b5201bad1ea087f707b
Glitch (glitch-hello-website) <none>
2024-03-16 20:05:33 +0000

    🍼🗃 Checkpoint ./script.js:65859954/628

---
+++ script.js
@@ -0,0 +1,11 @@
+// Define a variable for & console log:
+
+// 1. A string with your name
+
+// 2. Your lucky number
+
+// 3. Are you in class today? Use a boolean 
+
+// 4. An array of your 5 favorite animals
+
+// 5. An array with 
