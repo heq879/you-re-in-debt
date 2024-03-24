e9970f84156bc126a23ebdcd1c7a3297d7a500bd
Glitch (glitch-hello-website) <none>
2024-03-24 23:29:56 +0000

    🌂🌯 Checkpoint ./.glitch-assets:64181659/740 ./index.html:64181659/628 ./script.js:64181659/1364

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ console.log("Hello world");
 
 
 
-
 // PART A: 
+
+// PART X: Changing content
+
+// 1. Change the heading, the paragraph, and the image on the page to be about a topic of your choosing.
+document.querySelector("h1").innerHTML = "New Title";
+document.querySelector("p").innerHTML = "This is a new paragraph.";
+document.querySelector("img").src = "https://cdn.glitch.global/c61c7ecd-c992-456b-8136-a7497de5a768/koala.png?v=1711322726731";
+
+// 2. Add a new paragraph element to the page using JS
