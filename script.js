b7f1548fe58d8180c75f52b88f4c246d4e523afd
Glitch (glitch-hello-website) <none>
2024-03-24 23:41:49 +0000

    🚊🎢 Checkpoint ./script.js:64181659/1497 ./style.css:64181659/67 ./index.html:64181659/1891

--- script.js
+++ script.js
@@ -6,11 +6,24 @@ console.log("Hello world");
 
 // PART A: 
 
-// PART X: Changing content
+
+// PART X: Adding & changing content
 
 // 1. Change the heading, the paragraph, and the image on the page to be about a topic of your choosing.
-document.querySelector("h1").innerHTML = "New Title";
-document.querySelector("p").innerHTML = "This is a new paragraph.";
-document.querySelector("img").src = "https://cdn.glitch.global/c61c7ecd-c992-456b-8136-a7497de5a768/koala.png?v=1711322726731";
 
-// 2. Add a new paragraph element to the page using JS
+// 2. Only using JS, change the font size of your header to be larger
+
+// 3. In CSS, there is a rule with the selector "blue". Using JS, add the class "blue" to your header and paragraph to apply the existing CSS rules to them.
+
+// 4. Using only JS, add a new paragraph element with additional content related to your topic to the page
+
+// document.querySelector("h1").innerHTML = "New Title";
+// document.querySelector("p").innerHTML = "This is a new paragraph.";
+// document.querySelector("img").src = "https://cdn.glitch.global/c61c7ecd-c992-456b-8136-a7497de5a768/koala.png?v=1711322726731";
+
+
+
+
+// PART X
+
+// 1. Using event listeners and functions, make the color of the heading change on hover (mousein)
