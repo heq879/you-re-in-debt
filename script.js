a186e0b16e5d1673ee9802014f88a85502f73f89
Glitch (glitch-hello-website) <none>
2024-03-29 14:08:48 +0000

    🐬🛍 Checkpoint ./script.js:61015588/1158

--- script.js
+++ script.js
@@ -8,17 +8,38 @@ console.log("Hello world");
 // PART A: Conditionals 
 
 // 1. Create a variable with your name. Write a conditional if statement that console logs "My name is [your name]" when your variable is your name. Try to reuse your variable in your message as well, instead of just typing your name
+var myName = "Maee";
+
+if (myName === "Maee") {
+    console.log("My name is " + myName);
+} else {
+    console.log("This is not my name.");
+}
 
 // 2. Create a variable about whether it's the weekend or not (use a boolean). Write a conditional if else statement that console logs "It's the weekend :)" if it is, and console logs "It's not the weekend :(" if it's not. Try changing your variable to check if both states work.
+var isWeekend = false;
+
+if (isWeekend) {
+    console.log("It's the weekend :)");
+} else {
+    console.log("It's not the weekend :(");
+}
 
 // 3. Create a variable with an age (use a number). Write a conditional else if statement that console logs "You are old enough" if the age is ove 21, "You are underage" if the age is under 21, and "Congrats on turning 21!" if the age is 21.
+var myAge = 21
 
-
-
+if (myAge > 21) {
+    console.log("You are old enough");
+} else if (myAge < 21) {
+    console.log("You are underage");
+} else {
+    console.log("Congrats on turning 21!");
+}
 
 // PART B: Functions
 
 // 1. Write a function that console logs a message. Run the function to see the message in the console.
+
 
 // 2. Write a more dynamic function that has a parameter inside of the parentheses. Have it console log "I love the movie [movie name]". Use the argument to populate the movie name. Run the function 3 different times to console log 3 versions of the message.
 
