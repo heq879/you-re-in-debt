8cf67a4c372a894353ab03d19a90783848ef2796
Glitch (glitch-hello-website) <none>
2024-03-29 01:20:14 +0000

    🎸🏡 Checkpoint ./script.js:64181659/2045 ./style.css:64181659/56 ./index.html:64181659/184

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ console.log("Hello world");
 
 
 
+
 // PART A: Conditionals 
 
 // 1. Create a variable with your name. Write a conditional if statement that console logs "My name is [your name]" when your variable is your name. Try to reuse your variable in your message as well, instead of just typing your name
@@ -12,29 +13,6 @@ console.log("Hello world");
 
 // 3. Create a variable with an age (use a number). Write a conditional else if statement that console logs "You are old enough" if the age is ove 21, "You are underage" if the age is under 21, and "Congrats on turning 21!" if the age is 21.
 
-
-let myName = "Defne";
-if (myName == "Defne") {
-  console.log("My name is " + myName)
-}
-
-
-let itIsTheWeekend = false;
-if (itIsTheWeekend == true) {
-  console.log("It's the weekend :)")
-} else {
-  console.log("It's not the weekend :(")
-}
-
-
-let myAge = 20;
-if (myAge > 21) {
-  console.log("You are old enough")
-} else if (myAge < 21) {
-  console.log("You are underage")
-} else if (myAge == 21) {
-  console.log("Congrats on turning 21!")
-}
 
 
 
@@ -46,29 +24,7 @@ if (myAge > 21) {
 
 // 3. Write a function with parameters that does a mathematical calculation and returns an answer. Create 3 variable that holds your function and gives its parameters different values. Console log each of these variables to see each of their values.
 
-function myMessage() {
-  console.log("Hello there");
-};
-myMessage();
 
-function myMovie(movieName) {
-  console.log("I love the movie " + movieName);
-};
-myMovie("Pulp Fiction");
-myMovie("Poor Things");
-myMovie("Dune");
-
-function answer(num1, num2) {
-  return num1 * num2;
-}
-
-let myResult = answer(3, 7);
-let mySecondResult = answer(45, 87);
-let myThirdResult = answer(45, 87);
-
-console.log(myResult);
-console.log(mySecondResult);
-console.log(myResult);
 
 
 // PART C: The DOM
@@ -77,59 +33,23 @@ console.log(myResult);
 
 // 2. Only using JS, change the image.
 
-// 3. Only using JS, change the font size of your header to be larger
+// 3. Only using JS, change the font size of the first paragraph to be larger
 
-// 4. Only using JS, change the background color of the page
+// 4. Only using JS, change the font of the second paragraph
 
-// 5. In CSS, there is a rule with the selector "blue". Using JS, apply this CSS rule to your heading. (You may change the color/name)
+// 5. Only using JS, change the background color of the page
 
-// 6. In CSS, add a new rule that rounds corners to a new class. Add this class to the image.
+// 6. In CSS, there is a rule with the selector "blue". Using JS, apply this CSS rule to your paragraph. (You may change the color/name)
 
-document.querySelector("h1").innerHTML = "New Title";
-document.querySelector("img").src = "https://cdn.glitch.global/c61c7ecd-c992-456b-8136-a7497de5a768/koala.png?v=1711322726731";
-document.querySelector("h1").style.fontSize = "40px";
-document.querySelector("body").style.backgroundColor = "azure";
-document.querySelector("h1").classList.add("blue");
-document.querySelector("img").classList.add("round");
-
-
-// ??? to be added to a section (createElement & appendChild)
-// 6. Using only JS, add a new paragraph element with additional content related to your topic to the page
+// 7. In CSS, add a new rule that rounds corners to a new class. Using JS, add this class to the image.
 
 
 
 
-
-// PART X
+// PART D: Events
 
 // 1. Using event listeners and functions, make the copy of the title change once you click on it
 
-// 2. Inside of your function, add one more line that makes the color of the title change on click as well
+// 2. Inside of your existing function, add one more line that makes the color of the title change on the same click as well
 
 // 3. Once you click on the button, have the image change to a different image.
-
-// 4. 
-
-let titleText = document.querySelector('h1');
-titleText.addEventListener('click', myFunction);
-
-function myFunction() {
-     titleText.innerHTML = "Third title";
-     titleText.style.color = "orange";
-}
-
-
-let myImage = document.querySelector("img");
-let myButton = document.querySelector("#changeImage");
-myButton.addEventListener('click', imageChange);
-
-function imageChange() {
-  myImage.src = "https://cdn.glitch.global/c61c7ecd-c992-456b-8136-a7497de5a768/nycimage.png?v=1711325220939";
-}
-
-
-// if ('click', myImage) {
-//   document.querySelector("body").style.backgroundColor = "gainsboro"
-// } else {
-//   document.querySelector("body").style.backgroundColor = "azure"
-// }
