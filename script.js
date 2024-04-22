1e2c3c1b364ae751609103e0d27c97d609752276
Glitch (glitch-hello-website) <none>
2024-04-22 23:56:48 +0000

    📚🏂 Checkpoint ./script.js:61015588/1333 ./index.html:61015588/1922

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function updateOrderTime() {
     var orderTimeElement = document.getElementById('order-time');
     var currentDate = new Date();
+    var year = currentDate.getFullYear().toString();
+    var month = (currentDate.getMonth() + 1).toString().padStart(2, '0'); // Adding 1 as getMonth() returns zero-based month index
+    var day = currentDate.getDate().toString().padStart(2, '0');
     var hours = currentDate.getHours().toString().padStart(2, '0');
     var minutes = currentDate.getMinutes().toString().padStart(2, '0');
     var seconds = currentDate.getSeconds().toString().padStart(2, '0');
-    var currentTime = hours + ":" + minutes + ":" + seconds;
+    var currentTime = year + month + day + hours + minutes + seconds;
     orderTimeElement.innerText = currentTime;
 }
 
-// Update order time every second
 setInterval(updateOrderTime, 1000);
+
