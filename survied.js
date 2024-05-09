cc6540d57ea131e68fb48ab9b6e0fb331e669110
Glitch (glitch-hello-website) <none>
2024-05-09 03:42:27 +0000

    🌁⛳️ Checkpoint ./survied.html:61015588/754 ./survied.css:61015588/8213 ./survied.js:61015588/2765 ./.glitch-assets:61015588/1915

--- survied.js
+++ survied.js
@@ -1,16 +1,23 @@
-function generateBox(content) {
+function generateBox(content, imageUrl) {
     // Create a new box element
     var newBox = document.createElement("div");
     newBox.classList.add("box");
 
     // Set random position
-    var xPosition = Math.random() * (window.innerWidth - 120);
-    var yPosition = Math.random() * (window.innerHeight - 120);
+    var xPosition = Math.random() * (window.innerWidth - 220); // Adjust for box width
+    var yPosition = Math.random() * (window.innerHeight - 220); // Adjust for box height
     newBox.style.left = xPosition + "px";
     newBox.style.top = yPosition + "px";
 
-    // Set content
-    newBox.textContent = content;
+    // Create text element
+    var textElement = document.createElement("span");
+    textElement.textContent = content;
+    newBox.appendChild(textElement);
+
+    // Create img element
+    var img = document.createElement("img");
+    img.setAttribute("src", imageUrl);
+    newBox.appendChild(img);
 
     // Append the new box to the body
     document.body.appendChild(newBox);
