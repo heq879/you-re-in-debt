1ac84ce79cfcef57b71e9914304e166748b697b3
Glitch (glitch-hello-website) <none>
2024-05-09 04:18:42 +0000

    📰🚜 Checkpoint ./die.html:61015588/144 ./.glitch-assets:61015588/792 ./monday.html:61015588/176 ./monday.js:61015588/179 ./tuesday.html:61015588/10343 ./index.html:61015588/85

--- monday.js
+++ monday.js
@@ -89,7 +89,7 @@ function addToPay(price) {
   // Check if total pay amount exceeds 100
   if (totalPayAmount > 100) {
     // Change the image source
-    img.src = "https://cdn.glitch.global/a458af96-8264-4b0d-9750-e491a70291d9/payme.jpg?v=1715225219071";
+    img.src = "https://cdn.glitch.global/a458af96-8264-4b0d-9750-e491a70291d9/died.png?v=1715227942776";
   }
 }
 
